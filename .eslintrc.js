58d54738a7658a959f3c30dcee2651a748e30455
lupath <849104822@qq.com>
2017-08-07 17:58:50 +0800

    [chore]修改 eslint 配置项

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,17 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 分号结尾
+    "semi": [2, "always"],
+    // 引号类型
+    "quotes": [1, "double"],//引号类型 `` "" ''
+    //强制使用一致的缩进 第二个参数为 "tab" 时，会使用tab，
+    // if while function 后面的{必须与if在同一行，java风格。
+    "brace-style": [2, "1tbs", { "allowSingleLine": true }],
+    // // 不允许空格和 tab 混合缩进
+    "no-mixed-spaces-and-tabs": 0,
+    "no-tabs": "off",
+    "indent": [2, "tab"]//缩进风格
   }
 }
